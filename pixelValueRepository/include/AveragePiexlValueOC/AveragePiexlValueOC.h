1bf8eb8c178311776e3e036d3a7814e116f068bb
luoxianfu <24320182203245@stu.xmu.edu.cn>
2020-12-29 10:25:58 +0800

    first commit
    Signed-off-by: luoxianfu <24320182203245@stu.xmu.edu.cn>

---
+++ pixelValueRepository/include/AveragePiexlValueOC/AveragePiexlValueOC.h
@@ -0,0 +1,14 @@
+//
+//  AveragePiexlValueOC.h
+//  AveragePiexlValueOC
+//
+//  Created by 罗贤甫 on 2020/12/28.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+using namespace cv;
+@interface AveragePiexlValueOC : NSObject
+- (double) PixelValueOC: (UIImage *) image;
+@end
